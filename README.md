b65ed88b855341c7ea9708babae90cf2fe2c0ed6
monthwolf <52775320+monthwolf@users.noreply.github.com>
2024-03-09 14:43:45 +0000

    update

--- README.md
+++ README.md
@@ -1,17 +1,10 @@
-# 5e.tools
+# 5etools自动化翻译项目
+> 本项目fork自https://github.com/5etools-translated/5etools-translated.github.io/tree/translations
 
-Visit the [main site](https://5e.tools/index.html) or go to the unofficial GitHub [mirror](index.html).
-
-[Join the 5etools Discord here!](https://discord.gg/5etools)
-
-## Help and Support
-
-Please see [our wiki](https://wiki.tercept.net/) for FAQs, installation guides, supported integrations, and more.
-
-## Contributing
-
-See [CONTRIBUTING.md](CONTRIBUTING.md).
-
-## License
-
-This project is licensed under the terms of the MIT license.
+# 项目修复与更新
+- 修复了自动deepl翻译无法捕捉翻译框的错误
+- 专注于中文自动化翻译，对中文进行了更好的支持
+- 修复了deepl自动翻译时翻译结果不全的问题
+- 修复了全角括号造成的变量无法替换问题
+- 增加了GPT翻译方式，支持自定义代理地址和自定义模型调用
+- 增加环境变量，更新了translate工作流
---
+++ translation/clean.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Delete all broken translations')
+parser.add_argument('files', type=str, nargs='*')
+parser.add_argument('--maxfiles', type=int, default=150)
+args = parser.parse_args()
+
+start=0
+#print(args.files)
+def clean_files(log:str,logs:list):
+	num=args.maxfiles
+	for file in args.files[start:]:
+		with open(file,"r+") as data:
+			#清理包含未替换变量、输出不完整、未翻译、输出丢失变量的错误翻译映射
+			text=re.sub(r'.*?(%\d+%|%num%).*\n|.*?\{\d\}.*\n|.*?:([^a-zA-Z\n]+).*?(\[\.*\].*|[a-zA-Z]+",?)\n|".*?:[^\u4e00-\u9fa5]+",?\n|.*?\{.*?\}.*?:[^\{\}\n]+",?\n', '', data.read())
+			text=re.sub(r',\n\}','\n}',text)
+			data.seek(0)
+			data.truncate()
+			data.write(text)
+			logs[1]=file
+			log.seek(0)
+			log.truncate()
+			log.write('\n'.join(logs))
+			data.close()
+			num-=1
+			print(data.name+"清理完毕")
+		if(num==0):
+			break
+	log.close()
+if __name__ == "__main__":
+	try:
+		with open('log.txt','r+') as log:
+			logs=str.split(log.read(),'\n')
+			if len(logs)==2 and logs[0]==str(args.files):
+				# print(logs[1])
+				# print(args.files.index(logs[1]))
+				start=args.files.index(logs[1])
+			else:
+				logs[0]=str(args.files)
+			clean_files(log,logs)
+	except FileNotFoundError:
+		log=open('log.txt','w+')
+		logs=['','']
+		logs[0]=str(args.files)
+		clean_files(log,logs)
