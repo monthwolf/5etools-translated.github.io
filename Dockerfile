f5008e14dbb35a5119b11a0bfbc9eb6b84226582
monthwolf <52775320+monthwolf@users.noreply.github.com>
2024-03-20 15:29:31 +0000

    delete

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM ghcr.io/5etools-mirror-2/5etools-img:latest
-
-COPY . /var/www/localhost/htdocs/
--- node/rm.js
+++
@@ -1,7 +0,0 @@
-import {rmDirRecursiveSync} from "./util.js";
-
-if (process.argv.length < 3) throw new Error(`An argument is required!`);
-
-const tgt = process.argv[2];
-console.log(`Removing: ${tgt}`);
-rmDirRecursiveSync(tgt);
